60fbbcc95ddaf59d807f97cb504ebb1cba42d2e7
Alex Merecka <merecka@gmail.com>
2019-08-31 16:14:59 -0600

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,27 +3,18 @@ require 'pry'
 class UsersController < ApplicationController
 
 	def new
+		@user = User.new
 	end
 
-
 	def create
-	   if (user_params[:password] != nil && user_params[:password] != "") || (user_params[:password] != nil && user_params[:password_confirmation] !=nil && user_params[:password] == user_params[:password_confirmation])
-		   	@user = User.create(user_params)
+	   if (user_params[:password] != nil && user_params[:password_confirmation] !=nil) && user_params[:password] == user_params[:password_confirmation]
+	   		@user = User.create(user_params)
 			session[:user_id] = @user.id
 			redirect_to '/welcome'
-
 	   else
 	    	redirect_to '/users'
 	   end
 	end
-
-	# def create     
-	# 	@user = User.create(user_params) 
-	# 	binding.pry
-	# 	return redirect_to controller: 'users', action: 'new' unless @user.save     
-	# 		session[:user_id] = @user.id     
-	# 		redirect_to '/welcome' 
-	# end 
 
 	def welcome
 		@user = User.find_by(id: session[:user_id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-	has_secure_password validations: false
+	has_secure_password validations: true
 
 end
